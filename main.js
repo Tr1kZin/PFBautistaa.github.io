bd3c81bb063d7523ec19ae2899919f806f7efc26
Dylan Bautista <dylanbaucarc9@gmail.com>
2023-08-22 08:32:55 -0300

    pre-entrega3

--- main.js
+++ main.js
@@ -37,118 +37,179 @@ else{
     
 };
   */
-const dineroLavado =[`Dinero Total Lavado`];
-const usuarios =["mario", "juventus", "anastasio"]
-const actividadSemanal = [32, 12, 45, 98, 2, 43, 21];
-const menosActividad = 2;
-const indice = actividadSemanal.indexOf(menosActividad);
-if (indice !== -1) {
-    console.log(`El dia de menos actividad (${menosActividad} transacciones) fue el Viernes`);
-}
+// const dineroLavado =[`Dinero Total Lavado`];
+// const usuarios =["mario", "juventus", "anastasio"]
+// const actividadSemanal = [32, 12, 45, 98, 2, 43, 21];
+// const menosActividad = 2;
+// const indice = actividadSemanal.indexOf(menosActividad);
+// if (indice !== -1) {
+//     console.log(`El dia de menos actividad (${menosActividad} transacciones) fue el Viernes`);
+// }
 
 
 
- function menores(){
-    alert(`No puedes ingresar vuelve cuando tus ${edad} años sean 18.`)
+//  function menores(){
+//     alert(`No puedes ingresar vuelve cuando tus ${edad} años sean 18.`)
     
- }
- let descuento1 = 400;
- let descuento2 = 700;
- let descuento3 = 1000;
+//  }
+//  let descuento1 = 400;
+//  let descuento2 = 700;
+//  let descuento3 = 1000;
 
- const resta = (a, b) => a - b;
+//  const resta = (a, b) => a - b;
 
- const planes = [
-{plan: "1. Seguridad Baja / Se descuenta $400" },
-{plan: "2. Seguridad Media / Se descuenta $700" },
-{plan: "3. Seguridad Alta / Se descuenta $1000" },
- ]
+//  const planes = [
+// {plan: "1. Seguridad Baja / Se descuenta $400" },
+// {plan: "2. Seguridad Media / Se descuenta $700" },
+// {plan: "3. Seguridad Alta / Se descuenta $1000" },
+//  ]
   
- let edad = parseInt(prompt("Ingrese su edad"));
+//  let edad = parseInt(prompt("Ingrese su edad"));
 
 
-if (edad >= 18) {
-    alert("Bienvenido a DeepMarket")
-    let nombre = prompt("Ingrese su Nombre")
-    usuarios.push(nombre)
-    console.log(usuarios);
+// if (edad >= 18) {
+//     alert("Bienvenido a DeepMarket")
+//     let nombre = prompt("Ingrese su Nombre")
+//     usuarios.push(nombre)
+//     console.log(usuarios);
 
-    alert(`Hola ${nombre} , A continuacion le mostraremos nuestros planes de seguridad para realizar el lavado de dinero`)
+//     alert(`Hola ${nombre} , A continuacion le mostraremos nuestros planes de seguridad para realizar el lavado de dinero`)
     
-    for (const plans of planes) {
-        parseInt(alert( plans.plan ))
+//     for (const plans of planes) {
+//         parseInt(alert( plans.plan ))
         
-    }
-    let planselected = parseInt(prompt("Ingrese su Plan (1,2,3)"))
-    if (planselected === 1){
-        alert("Usted eligio el plan Seguridad Baja")
-        let dinero = parseInt(prompt("Ingrese el dinero a Lavar"))
-        if (dinero > 400){
-            alert(`A sus $${dinero} se le descontara lo acordado.`);
-            let resultado = resta(dinero,descuento1);
-            dineroLavado.push(resultado) 
-            console.log(dineroLavado);            
-            alert(`El total de $${resultado} sera lavado. ACEPTAS? `)
-            for (let i = 0; i < 100; i = i + 9) {
-                alert(`Lavando dinero ${i} %`)  
-                 }
-                 alert(`${nombre} su dinero, $${resultado} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
+//     }
+//     let planselected = parseInt(prompt("Ingrese su Plan (1,2,3)"))
+//     if (planselected === 1){
+//         alert("Usted eligio el plan Seguridad Baja")
+//         let dinero = parseInt(prompt("Ingrese el dinero a Lavar"))
+//         if (dinero > 400){
+//             alert(`A sus $${dinero} se le descontara lo acordado.`);
+//             let resultado = resta(dinero,descuento1);
+//             dineroLavado.push(resultado) 
+//             console.log(dineroLavado);            
+//             alert(`El total de $${resultado} sera lavado. ACEPTAS? `)
+//             for (let i = 0; i < 100; i = i + 9) {
+//                 alert(`Lavando dinero ${i} %`)  
+//                  }
+//                  alert(`${nombre} su dinero, $${resultado} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
 
-        }else{
-            alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
-        }
+//         }else{
+//             alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
+//         }
 
-    }else;
-    if (planselected === 2){
-        alert("Usted eligio el plan Seguridad Media");
-        let dinero2 = parseInt(prompt("Ingrese el dinero a lavar"))
-        if (dinero2 > 700){
-            alert(`A sus $${dinero2} se le descontara lo acordado.`);
-            let resultado2 = resta(dinero2,descuento2);
-            dineroLavado.push(resultado2) 
-            console.log(dineroLavado); 
-            alert(`El total de $${resultado2} sera lavado . ACEPTAS?`);
+//     }else;
+//     if (planselected === 2){
+//         alert("Usted eligio el plan Seguridad Media");
+//         let dinero2 = parseInt(prompt("Ingrese el dinero a lavar"))
+//         if (dinero2 > 700){
+//             alert(`A sus $${dinero2} se le descontara lo acordado.`);
+//             let resultado2 = resta(dinero2,descuento2);
+//             dineroLavado.push(resultado2) 
+//             console.log(dineroLavado); 
+//             alert(`El total de $${resultado2} sera lavado . ACEPTAS?`);
             
 
 
-            for (let i = 0; i < 100; i = i + 9) {
-                alert(`Lavando dinero ${i} %`)  
-                  }
-                 alert(` ${nombre} su dinero, $${resultado2} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
+//             for (let i = 0; i < 100; i = i + 9) {
+//                 alert(`Lavando dinero ${i} %`)  
+//                   }
+//                  alert(` ${nombre} su dinero, $${resultado2} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
 
-        }else{
-            alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
-        }       
-    }
+//         }else{
+//             alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
+//         }       
+//     }
 
-    if (planselected === 3){
-        alert("Usted eligio el plan Seguridad Alta");
-        let dinero3 = parseInt(prompt("Ingrese el dinero a lavar"))
-        if (dinero3 > 700){
-            alert(`A sus $${dinero3} se le descontara lo acordado.`);
-            let resultado3 = resta(dinero3,descuento3);
-            dineroLavado.push(resultado3) 
-            console.log(dineroLavado); 
-            alert(`El total de $${resultado3} sera lavado . ACEPTAS?`)
+//     if (planselected === 3){
+//         alert("Usted eligio el plan Seguridad Alta");
+//         let dinero3 = parseInt(prompt("Ingrese el dinero a lavar"))
+//         if (dinero3 > 700){
+//             alert(`A sus $${dinero3} se le descontara lo acordado.`);
+//             let resultado3 = resta(dinero3,descuento3);
+//             dineroLavado.push(resultado3) 
+//             console.log(dineroLavado); 
+//             alert(`El total de $${resultado3} sera lavado . ACEPTAS?`)
 
-            for (let i = 0; i < 100; i = i + 9) {
-                alert(`Lavando dinero ${i} %`)  
-                  }
-                 alert(`${nombre} su dinero, $${resultado3} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
+//             for (let i = 0; i < 100; i = i + 9) {
+//                 alert(`Lavando dinero ${i} %`)  
+//                   }
+//                  alert(`${nombre} su dinero, $${resultado3} ha sido lavado con exito. La Transaccion puede durar hasta 24hs.`)
 
-        }else{
-            alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
-        }  
+//         }else{
+//             alert(`Datos incorrectos. Recargue la pagina ${nombre}`)
+//         }  
        
-    }
+//     }
 
     
   
 
-}else{
- menores();
+// }else{
+//  menores();
  
+// }
+
+let carrito = [];
+let total = 0;
+
+document.getElementById("boton3").onclick = function(){
+    agregarAlCarrito(`Katana Yasuo`, 999, );
+    
+};
+document.getElementById("boton2").onclick = function(){
+    agregarAlCarrito(`Llavero Fizz`, 500)
+};
+document.getElementById("boton1").onclick = function(){
+    agregarAlCarrito(`Espada Aatrox`, 250)
+};
+
+const btnEliminarCarrito = document.getElementById("borrarCarrito");
+btnEliminarCarrito.addEventListener("click", function(){
+  localStorage.removeItem("carrito");
+  carrito.length= 0 ;
+    actualizarCarrito();
+});
+btnEliminarCarrito.addEventListener("click", function(){
+    localStorage.removeItem(total = 0)
+    total.length = 0;
+    actualizarCarrito();
+})
+
+if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+    total = parseFloat(localStorage.getItem('total'));
+    actualizarCarrito(); 
+
 }
+
+function actualizarCarrito() {
+    const carritoDiv = document.getElementById('carrito');
+    carritoDiv.innerHTML = '';
+       
+
+    carrito.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.innerText = `${producto.nombre} - $${producto.precio}`;
+        carritoDiv.appendChild(productoDiv); 
+        
+    });
+
+    const totalSpan = document.getElementById('total');
+
+    totalSpan.innerText = total.toFixed(2);
+ 
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    localStorage.setItem('total', total.toFixed(2));
+}
+
+function agregarAlCarrito(nombre, precio) {     
+    carrito.push({ nombre, precio });
+    total += precio;
+    actualizarCarrito();  
+}
+
+
 
 
 
